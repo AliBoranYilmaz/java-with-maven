bf03e5979513db2f5f824e5b4fde930e265c8e23
aliboranyilmaz <aliboranyilmaz@outlook.com>
2025-01-16 14:17:22 +0300

    pom.xml version updated to 6.3.0

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is bla bla40: " + currentTime);
+		  System.out.println("The current local time is bla bla41: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
