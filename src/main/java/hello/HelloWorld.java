a5ca50b763ab7adbfbf07f543d76592cc1b6a931
aliboranyilmaz <aliboranyilmaz@outlook.com>
2025-01-16 10:54:41 +0300

    Added bla bla21 (major) Version 2.0.0

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is bla bla20: " + currentTime);
+		  System.out.println("The current local time is bla bla21: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
