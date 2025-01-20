ca3e9a903f4baba618b21d9ee1f66b5f4b1fc09a
aliboranyilmaz <aliboranyilmaz@outlook.com>
2025-01-20 09:16:02 +0300

    Added bla bla52 in aliboran (patch)

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is bla bla51: " + currentTime);
+		  System.out.println("The current local time is bla bla52: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
